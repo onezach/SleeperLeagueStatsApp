0bec420964f5c053a6ae5ffc08da7e849aadebc0
onezach <87786580+onezach@users.noreply.github.com>
2024-12-01 20:14:53 -0600

    clean homepage standings

--- src/components/pages/Homepage.jsx
+++ src/components/pages/Homepage.jsx
@@ -1,42 +1,49 @@
-import { Container, Row, Col } from "react-bootstrap";
-
 export default function Homepage(props) {
   return (
-    <Container>
-      <h1>Overview</h1>
-      <Row style={{ margin: "0.2rem" }}>
-        <Col xs={1}>
-          <b>Rank</b>
-        </Col>
-        <Col xs={3}>
-          <b>Team</b>
-        </Col>
-        <Col xs={1}>
-          <b>Record</b>
-        </Col>
-        <Col xs={2}>
-          <b>Points For</b>
-        </Col>
-        <Col xs={2}>
-          <b>Max Points For</b>
-        </Col>
-      </Row>
-      {props.standings.map((team, rank) => (
-        <Row key={team.name} style={{ margin: "0.2rem" }}>
-          <Col xs={1}>{rank + 1}</Col>
-          <Col xs={3}>
-            <img alt="Team Logo" src={team.avatar} width="20" height="20" />{" "}
-            {team.name}
-          </Col>
-          <Col xs={1}>
-            {team.wins}
-            {"-"}
-            {team.losses}
-          </Col>
-          <Col xs={2}>{team.points_for}</Col>
-          <Col xs={2}>{team.max_points_for}</Col>
-        </Row>
-      ))}
-    </Container>
+    <div style={{ flexDirection: "column", margin: "1rem" }}>
+      <h1>Standings</h1>
+      <div
+        style={{
+          width: "100%",
+          overflowX: "auto",
+          WebkitOverflowScrolling: "touch",
+        }}
+      >
+        <table style={{ minWidth: "1000px", borderCollapse: "collapse" }}>
+          <thead>
+            <tr>
+              <th>Rank</th>
+              <th>Team</th>
+              <th>Record</th>
+              <th>Points For</th>
+              <th>Max Points For</th>
+            </tr>
+          </thead>
+          <tbody>
+            {props.standings.map((team, rank) => (
+              <tr key={team.name}>
+                <td>{rank + 1}</td>
+                <td>
+                  <img
+                    alt="Team Logo"
+                    src={team.avatar}
+                    width="20"
+                    height="20"
+                  />{" "}
+                  {team.name}
+                </td>
+                <td>
+                  {team.wins}
+                  {"-"}
+                  {team.losses}
+                </td>
+                <td>{team.points_for}</td>
+                <td>{team.max_points_for}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
   );
 }
